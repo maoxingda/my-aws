c7d2c996bca35ed8d025f9bf48f9aa34977a3807
maoxd <maoxingda@meican.com>
2021-08-26 23:56:59 +0800

    fix is_relpath:local:1: path: can't assign initial value for array.

--- s3/utils/is_relpath.zsh
+++ s3/utils/is_relpath.zsh
@@ -1,6 +1,6 @@
 function is_relpath() {
-    local path=$1
-    if [[ "${path:0:1}" != "/" && "${path:0:5}" != "s3://" ]]; then
+    local dir=$1
+    if [[ "${dir:0:1}" != "/" && "${dir:0:5}" != "s3://" ]]; then
         return 0
     fi
     return 1
