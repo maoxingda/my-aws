c3ec0abedea697316f80d0e4ee1f3399f7bbe34e
maoxd <maoxingda@meican.com>
2021-08-28 17:26:40 +0800

    Refactor srm.

--- s3/cmds/srm.zsh
+++ s3/cmds/srm.zsh
@@ -1,20 +1,22 @@
 function srm() {
     if ((s3_debug == 1)); then set -vx; fi
-    trap "if ((s3_debug == 1)); then set +vx; fi" EXIT
+    trap 'if ((s3_debug == 1)); then set +vx; fi' EXIT
 
-    local opt=""
+    local opt=''
     local dryrun=0
-    local include=""
+    local include=''
     local recursive=0
 
-    while getopts hdri: opt; do
+    while getopts 'hdri': opt; do
         case ${opt} in
         h)
             tip "Usage:"
-            tip "    $0 [-h] [-r] [-d] [-i <wildcard>] <S3Uri>" && echo
+            tip "    $0 [-h] [-r] [-d] [-i <wildcard>] <S3Uri>"
 
-            tip "    -h Print this message, then exit" && echo
+            echo
+            tip "    -h Print this message, then exit"
 
+            echo
             tip "    -r Command is performed on all files or objects under the specified directory or prefix."
             tip "    -d Displays the operations that would be performed using the specified command without actually running them."
             tip "    -i Don’t exclude files or objects in the command that match the specified pattern. See http://docs.aws.amazon.com/cli/latest/reference/s3/index.html#use-of-exclude-and-include-filters"
@@ -37,27 +39,32 @@ function srm() {
     done
     ((OPTIND > 1)) && shift $((OPTIND - 1))
 
+    typeset -a err
+
+    err[2]='Expect argument: <S3Uri>, got none.'
+    err[3]='Expect 1 positional argument: <S3Uri>, got:'
+
     if (($# == 0)); then
-        tip "Expect argument: <S3Uri>, got none"
-        return 1
+        tip "${err[2]}"
+        return 2
     fi
 
     if (($# > 1)); then
-        tip "Expect 1 positional argument: <S3Uri>, got: $#"
-        return 1
+        tip "${err[3]} $#"
+        return 3
     fi
 
-    local s3Uri=$1
+    local S3Uri="$1"
 
-    if [[ ${s3Uri:0:1} == "." ]]; then
-        tip "Bucket names must begin and end with a letter or number."
-        return 1
-
-    elif is_relpath "${s3Uri}"; then
-        s3Uri="${s3_pwd%/}/${s3Uri}"
+    if is_relpath "${S3Uri}"; then
+        if [[ ${s3_pwd} == '/' ]]; then
+            S3Uri="/${S3Uri}"
+        else
+            S3Uri="${s3_pwd}/${S3Uri}"
+        fi
     fi
 
-    cmd="aws s3 rm ${s3Uri}"
+    cmd="aws s3 rm s3:/${S3Uri}"
 
     ((dryrun)) && cmd="${cmd} --dryrun"
 
