5b0245c625fd61fa2173fcbd664191730647fdf3
maoxd <maoxingda@meican.com>
2021-08-28 13:49:43 +0800

    Refactor sls.

--- s3/cmds/sls.zsh
+++ s3/cmds/sls.zsh
@@ -5,10 +5,10 @@ function sls() {
     local opt
     local recursive=0
 
-    while getopts hr opt; do
+    while getopts 'hr' opt; do
         case ${opt} in
         h)
-            tip "Usage: $0 [-r] [s3Uri]"
+            tip "Usage: $0 [-r] [S3Uri]"
             return 0
             ;;
         r)
@@ -22,22 +22,22 @@ function sls() {
     ((OPTIND > 1)) && shift $((OPTIND - 1))
 
     if (($# > 1)); then
-        tip "Expect 0 or 1 positional argument: [s3Uri], got $#"
-        return 1
+        tip "Expect 0 or 1 positional argument: [S3Uri], got $#"
+        return 2
     fi
 
-    local s3Uri=$1
+    local s3_uri="$1"
 
-    if [[ -z "${s3Uri}" || "${s3Uri}" == "." ]]; then
-        s3Uri="${s3_pwd%/}/"
+    if [[ -z ${s3_uri} || ${s3_uri} == "." ]]; then
+        s3_uri="${s3_pwd%%/}"
 
-    elif is_relpath "${s3Uri}"; then
-        s3Uri="${s3_pwd%/}/${s3Uri%/}/"
+    elif is_relpath "${s3_uri}"; then
+        s3_uri="${s3_pwd%%/}/${s3_uri}"
     fi
 
-    cmd="aws s3 ls ${s3Uri} --human-readable"
+    cmd="aws s3 ls ${s3_uri} --human-readable"
 
     ((recursive)) && cmd="${cmd} --recursive"
 
-    eval "${cmd}"
+    eval "${cmd%%/}/"
 }
